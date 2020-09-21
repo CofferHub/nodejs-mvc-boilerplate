297993c5c7020618410fefabf778f22761661824
AtilaAssuncao <atilamoises718@gmail.com>
2020-09-21 13:58:10 -0300

    Resolve on issue #4

---
+++ src/database/config/database.js
@@ -0,0 +1,34 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: null,
+    database: 'database_development',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: 0,
+    define: {
+      timestamp: true,
+      underscored: true
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  }
+};
--- src/database/index.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require('sequelize');
-const dbConfig = require('../config/database');
-
-const connection = new Sequelize( dbConfig );
-
-module.exports = connection;
