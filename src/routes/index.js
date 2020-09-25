f0e0b6ef7c43104afd6ac8c874ebaf91de62d601
AtilaAssuncao <atilamoises718@gmail.com>
2020-09-24 22:08:35 -0300

    adjustment

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const UserController = require('../controllers/UserController');
 
 router.get('/', UserController.index);
 router.get('/view/:id', UserController.view);
-router.get('/update/:id', UserController.upate);
+router.get('/update/:id', UserController.viewUpdate);
 router.post('/create', UserController.create);
 router.put('/update/:id', UserController.update);
 router.delete('/delete/:id', UserController.delete);
