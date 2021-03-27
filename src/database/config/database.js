1cfff2543106084fc2deb953c46eed055924f8fe
AtilaAssuncao <atilamoises718@gmail.com>
2021-03-27 20:11:57 -0300

    Featre dotenv

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -18,17 +18,17 @@ module.exports = {
     }
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql'
+    username: '',
+    password: '',
+    database: '',
+    host: '',
+    dialect: ''
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql'
+    username: '',
+    password: '',
+    database: '',
+    host: '',
+    dialect: ''
   }
 };
